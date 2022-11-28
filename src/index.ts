b6c409bb815eedfa06d58fea908205a8a232ac7b
lou <53594864+ld-amaya@users.noreply.github.com>
2022-11-27 20:35:41 -0800

    Create and endpoint for user registration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import axios from 'axios';
+import dotenv from "dotenv";
+import { iDataDTO } from './models/iDataDTO';
+import authController from './controllers/authController';
+import { iUserID } from './models/iUserIDDTO';
+
+dotenv.config();
 
 const PORT = 8000;
 const app = Express();
@@ -9,8 +14,17 @@ const app = Express();
 app.use(bodyParser.json());
 app.use(cors());
 
+/**
+ * Provide user endpoint that returns the user id
+ */
+app.post("/register", async (req, res) => {
+    const user: iDataDTO = req.body;
+    const controller = new authController();
+    await controller.register(user, (results: iUserID) => {
+        res.send(results);    
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
-})
-
-
+});
